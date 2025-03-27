c4257e9915485389e6fd1a098c94e1c4bb7de45a
Noah Normand <noah.normand@student.junia.com>
2025-03-27 14:33:08 +0100

    Fin de la partie 4 modification de la bdd

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php require 'Models/pdo.php'?>
+<?php require 'Models/pdo.php'; ?>
+
 <?php
 $query = $dbPDO->query('SELECT * FROM manga ORDER BY année');
 $manga = $query->fetchAll(PDO::FETCH_ASSOC);
@@ -8,9 +9,19 @@ $manga = $query->fetchAll(PDO::FETCH_ASSOC);
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <h1>Top manga</h1>
-<?php  foreach ($manga as $mangas) { ?>
-<li><?= $mangas['titre'] ?></li>
-<?= $mangas['année'] ?>
-
-<?php } ?>
+    <title>Top Manga</title>
+</head>
+<body>
+<h1>Top Manga</h1>
+<ul>
+    <?php foreach ($manga as $mangas) { ?>
+        <li>
+            <a href='manga/manga.php?id=<?= $mangas['idmanga'] ?>'>
+                <?= $mangas['titre'] ?>
+            </a>
+            <?= $mangas['année'] ?>
+        </li>
+    <?php } ?>
+</ul>
+</body>
+</html>
---
+++ manga/manga.php
@@ -0,0 +1,17 @@
+<?php
+require '../Models/pdo.php';
+
+$id = $_GET['id'];
+
+    $requete = $dbPDO->prepare('SELECT * FROM manga,auteur,personnage WHERE idmanga = :id and manga.idmanga = auteur.idauteur and personnage.idperso=:id');
+    $requete->bindParam(':id', $id, PDO::PARAM_INT);
+    $requete->execute();
+    $manga = $requete->fetch(PDO::FETCH_ASSOC);
+     echo '<h1>'.$manga['titre']. '</h1>';
+        echo '<p>'.$manga['nom']." - ".$manga['année'].'</p>';
+
+
+        echo '<br>' . $manga['description'] . '<br>';
+        echo '<br> Personnage : ' . $manga['perso'] . '<br>';
+
+?>
